4ad1b2cad7e410426143758b63adcecc7fcfac9a
ikneb <ikneb@gmail.com>
2016-02-17 14:14:53 +0200

    add key and value in map input on console

--- src/Exception/MapTest.java
+++ src/Exception/MapTest.java
@@ -1,7 +1,26 @@
 package Exception;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * Created by Benki on 17.02.2016.
  */
 public class MapTest {
+    public static void main(String[] args) {
+
+
+        Map<String, String> map = new HashMap<>();
+        map.put("Murzic", "cat");
+        map.put("Fozya","mouse");
+        map.put("Barsik","dog");
+
+        Iterator<Map.Entry<String,String>> iter = map.entrySet().iterator();
+
+        while(iter.hasNext()){
+            Map.Entry maps = iter.next();
+            System.out.println(maps.getKey() + " " + maps.getValue());
+        }
+    }
 }
